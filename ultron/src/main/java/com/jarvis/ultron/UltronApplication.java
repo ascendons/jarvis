9a4f73232d85cf3f60b844251b27d579b49fbe69
pankajThakur1874 <pankajthakur1874@gmail.com>
2025-03-19 19:52:37 +0530

    Initial commit

---
+++ ultron/src/main/java/com/jarvis/ultron/UltronApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis.ultron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltronApplication.class, args);
+    }
+
+}
